d53ec940dfb9740053396bc7651b2b00113ddd25
vinicioscsoares <vinicioscsoares@gmail.com>
2024-05-17 18:26:18 -0300

    Models criado

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from contact.models import Contact
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name','phone','email','created_date')
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name','phone', 'email',
+    list_per_page= 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone', 'email'
+    
+    #list_filter = 'created_date',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model): 
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    created_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name} {self.email}'
+    
